9d09d464f1198cd3ec978e1d00f876732ecbe5b8
tylercchase <tylercchase@gmail.com>
2022-03-12 20:29:07 -0900

    Init
    Co-authored-by: Solomon Himelbloom <TechSolomon@users.noreply.github.com>

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+// main.cpp
+// Tyler Chase
+// Solomon Himelbloom
+// 2022-03-12
+//
+// For CS 321 Spring 2022
+// Final Project: Octoshell
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <cstdlib>
+
+// userPause
+// Wait for user to press ENTER: read all chars through first newline.
+void userPause()
+{
+    std::cout.flush();
+    while (std::cin.get() != '\n') ;
+}
+
+// Main program
+int main() {
+    std::cout << "Hello, world." << std::endl;
+
+    // Wait for user
+    std::cout << "Press ENTER to quit ";
+    userPause();
+}
